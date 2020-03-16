3254cc78eb324aa9cac5ea7fbbdf3a268f4806bd
Michael Kutzer <kutzer@usna.edu>
2020-03-16 11:12:46 -0400

    Update getClassifierImages.m

--- getClassifierImages.m
+++ getClassifierImages.m
@@ -1,13 +1,17 @@
-function classifierDataFolder = getClassifierImages(cam,W,idx,n)
+function classifierDataFolder = getClassifierImages(prv,W,idx,n)
+% GETCLASSIFIERIMAGES takes a series of images for object classification.
+%   classifierDataFolder = getClassifierImages(prv,W,idx,n)
+%
+%   M. Kutzer, 27Feb2017, USNA
 
-%% Get camera preview
-prv = preview(cam);
-
+% Updates
+%   16Mar2020 - Updated to use preview object instead of camera object.
+%   16Mar2020 - Updated to add line feed & image number in pop-up.
 %% Get classifier data
 % Define folder name
 classifierDataFolder = sprintf('Classifier Data, %s',W{idx});
 % Create directory if it doesn't exist
-if ~isdir(classifierDataFolder)
+if ~isfolder(classifierDataFolder)
     mkdir(classifierDataFolder);
 end
 
@@ -20,12 +24,13 @@ for i = 1:n
     % Prompt user to put object in FOV
     uiwait(...
         msgbox(...
-        sprintf(['Place the “%s” in the camera FOV...',...
-        '[Enter to Continue]'],W{idx}),'Grab Image'));
+        sprintf(['Place the “%s” in the camera FOV (%d of %d)...',...
+        '[Enter to Continue]'],W{idx},i,n),'Grab Image'));
     % Allow MATLAB to update
     drawnow
     % Get image
     im = get(prv,'CData');
     % Save image to classifier data folder
     imwrite(im,fullfile(classifierDataFolder,fname),'png');
+    fprintf('\n');
 end
