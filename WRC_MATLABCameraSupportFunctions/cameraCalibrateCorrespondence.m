3e996056683de4a1d1171e8551f1e5722b8f6971
Michael Kutzer <kutzer@usna.edu>
2022-09-14 13:05:10 -0400

    Update cameraCalibrateCorrespondence.m

--- WRC_MATLABCameraSupportFunctions/cameraCalibrateCorrespondence.m
+++ WRC_MATLABCameraSupportFunctions/cameraCalibrateCorrespondence.m
@@ -3,26 +3,35 @@ function varargout = cameraCalibrateCorrespondence(pName,varargin)
 % returning information to establish a correspondence with other data
 % set(s).
 %   [A_c2m,H_f2c,corIndexes,camParams,info] =...
-%                                     cameraCalibrateCorrespondence(pName)
+%                                     CAMERACALIBRATECORRESPONDENCE(pName)
 %
 %   [...] = cameraCalibrateCorrespondence(___,squareSize)
 %
 %   [...] = cameraCalibrateCorrespondence(___,lensType)
 %
+%   [...] = cameraCalibrateCorrespondence(___,bName)
+%
+%   [...] = cameraCalibrateCorrespondence(___,fExt)
+%
 %   Input(s)
-%       pName      - [OPTIONAL] string defining folder path containing m
-%                    calibration images OR a cell array defining
-%                    calibration images
+%       pName      - [OPTIONAL] character array defining folder path 
+%                    containing m calibration images OR a cell array 
+%                    defining calibration images
 %       squareSize - [OPTIONAL] positive scalar value defining square size 
 %                    in millimeters
 %       lensType   - [OPTIONAL] string argument defining lens type
 %                    {'standard','fisheye'}
+%       bName      - [OPTIONAL] character array defining the base filename  
+%                    of each image
+%       fExt       - [OPTIONAL] character array defining the file extension
+%                    of each image. NOTE: fExt must include '.' as the
+%                    first character (e.g. '.png')
 %
 %   Output(s)
 %       A_c2m      - 3x3 array defining camera intrinsics
 %       H_f2c      - n-element cell array containing extrinsics
 %       corIndexes - m-element index array noting image index for 
-%                      defining correspondence
+%                    defining correspondence
 %       camParams  - camera parameters natively returned by MATLAB
 %       info       - structured array containing additional information
 %           info.imageNames - cell array containing images used in
@@ -40,9 +49,16 @@ squareSize  = [];
 lensType    = [];
 imageNames  = {};
 corIndexes  = [];
+bName       = '';
+fExt        = '';
+
+%% Set default output(s)
+for i = 1:nargout
+    varargout{i} = [];
+end
 
 %% Check input(s)
-narginchk(0,3);
+narginchk(0,5);
 
 if nargin < 1
     pName = uigetdir;
@@ -60,6 +76,7 @@ switch lower( class(pName) )
 end
 
 for i = 1:numel(varargin)
+    % Square size
     if isnumeric( varargin{i} )
         if numel(varargin{i}) == 1 && nnz(varargin{i} > 0) == 1
             squareSize = varargin{i};
@@ -72,134 +89,25 @@ for i = 1:numel(varargin)
             lensType = lower( char( varargin{i} ) );
             continue
     end
+    
+    switch class( varargin{i} )
+        case {'char','string'}
+            if numel(varargin{i}) > 1
+                if matches(varargin{i}(1),'.')
+                    fExt = varargin{i};
+                else
+                    bName = varargin{i};
+                end
+                continue
+            end
+    end
 
     warning('Ignoring input %d.',i+1);
 end
 
 %% Get image names
 if isempty(imageNames)
-    % Get supported image formats
-    imExts = {};
-    imExtStruct = imformats;
-    for i = 1:numel(imExtStruct)
-        imExt = imExtStruct(i).ext;
-        for j = 1:numel(imExt)
-            imExts{end+1} = imExt{j};
-        end
-    end
-
-    % Get folder contents & identify images
-    D = dir(pName);
-    idx = find(~[D.isdir]);
-    tfImage = false(1,numel(idx));
-    for i = 1:numel(idx)
-        [~,bName{i},fExt{i}] = fileparts( D( idx(i) ).name );
-        tfImage(i) = nnz( matches(imExts,fExt{i}(2:end)) ) > 0;
-    end
-
-    % Isolate images
-    bName = bName(tfImage);
-    fExt = fExt(tfImage);
-
-    % Display available image formats
-    u_fExt = unique(fExt);
-    for i = 1:numel(u_fExt)
-        n_fExt(i) = nnz( matches(fExt,u_fExt{i}) );
-        fprintf('*%s - %d found.\n',u_fExt{i},n_fExt(i));
-    end
-
-    % Select most prevalent format
-    % TODO - prompt user?
-    [~,idx] = max(n_fExt);
-    tfFormat = matches(fExt,u_fExt{idx});
-
-    bName = bName(tfFormat);
-    fExt  =  fExt(tfFormat);
-
-    % Keep the unique image format
-    fExt = fExt{1};
-
-    % Isolate base names from image numbers
-    n = numel(bName);
-    baseName   =  cell(1,n);
-    imgNumStr  =  cell(1,n);
-    imgNum     = zeros(1,n);
-    nNumStr    = zeros(1,n);
-    tfNumbered = false(1,n);
-    for i = 1:n
-        splitStr = regexp(bName{i},'\_','split');
-        switch numel(splitStr)
-            case 2
-                baseName{i}   = splitStr{1};
-                imgNumStr{i}  = splitStr{2};
-                imgNum(i)     = str2double(imgNumStr{i});
-                nNumStr(i)    = numel(imgNumStr{i});
-                tfNumbered(i) = true;
-            otherwise
-                fprintf('Ignoring %s%s - Bad Format.\n',bName{i},fExt);
-        end
-    end
-
-    baseName  =  baseName(tfNumbered);
-    imgNumStr = imgNumStr(tfNumbered);
-    imgNum    =    imgNum(tfNumbered);
-
-    % Find unique base names
-    u_baseName = unique(baseName);
-    for i = 1:numel(u_baseName)
-        n_baseName(i) = nnz( matches(baseName,u_baseName{i}) );
-        fprintf('%s_*%s - %d found.\n',u_baseName{i},fExt,n_baseName(i));
-    end
-
-    % Select most prevalent baseName
-    % TODO - prompt user?
-    [~,idx] = max(n_baseName);
-    tfbaseName = matches(baseName,u_baseName{idx});
-
-    baseName  =  baseName(tfbaseName);
-    imgNumStr = imgNumStr(tfbaseName);
-    imgNum    =    imgNum(tfbaseName);
-    nNumStr   =   nNumStr(tfbaseName);
-
-    % Keep the uniqe base name
-    baseName = baseName{1};
-
-    % TODO - check nNumStr for consistency
-
-    % Sort images
-    [~,idx] = sort(imgNum);
-
-    %baseName  =  baseName(idx);
-    imgNumStr = imgNumStr(idx);
-    imgNum    =    imgNum(idx);
-    nNumStr   =   nNumStr(idx);
-
-    % Define image names
-    if imgNum(1) == 0
-        i0 = 0;
-    else
-        i0 = 1;
-    end
-    i1 = imgNum(end);
-
-    indexVals = i0:i1;
-    for i = 1:numel(indexVals)
-        % Check if index is included
-        tfImage = imgNum == indexVals(i);
-        switch nnz(tfImage)
-            case 1
-                %fmtStr = sprintf('0%d',nNumStr(tfImage));
-                imageName = sprintf('%s_%s%s',baseName,imgNumStr{tfImage},fExt);
-                imageNames{end+1} = fullfile(pName,imageName);
-                corIndexes(end+1) = i;
-
-                fprintf('\t%s - Correspondence Index %d\n',imageName,corIndexes(i));
-            case 0
-                % No image exists
-            otherwise
-                % Multiple images exist
-        end
-    end
+    [imageNames,corIndexes] = findImageSet(pName,bName,fExt);
 else
     % Check image names
     tfKeep = isfile(imageNames);
@@ -210,6 +118,11 @@ else
 end
 
 fprintf('%d candidate correspondence images.\n',numel(imageNames));
+
+if numel(imageNames) < 1
+    warning('No valid images found.');
+    return
+end
 
 %% Process images
 % Detect checkerboards in images
@@ -268,27 +181,34 @@ if isempty(lensType)
 end
 
 % Calibrate the camera
-switch lensType
-    case 'standard'
-        % Standard camera calibration
-        fprintf('\nCalibrating using standard camera model...');
-        [camParams, imagesUsed, estimationErrors] = ...
-            estimateCameraParameters(P_m, P_g, ...
-            'EstimateSkew', false, 'EstimateTangentialDistortion', false, ...
-            'NumRadialDistortionCoefficients', 2, 'WorldUnits', 'millimeters', ...
-            'InitialIntrinsicMatrix', [], 'InitialRadialDistortion', [], ...
-            'ImageSize', [mrows, ncols]);
-        fprintf('COMPLETE\n\n');
-    case 'fisheye'
-        % Fisheye camera calibration
-        fprintf('\nCalibrating using fisheye camera model...');
-        [camParams, imagesUsed, estimationErrors] = ...
-            estimateFisheyeParameters(P_m, P_g, ...
-            [mrows, ncols], ...
-            'EstimateAlignment', true, ...
-            'WorldUnits', 'millimeters');
-        fprintf('COMPLETE\n\n');
+try
+    switch lensType
+        case 'standard'
+            % Standard camera calibration
+            fprintf('\nCalibrating using standard camera model...');
+            [camParams, imagesUsed, estimationErrors] = ...
+                estimateCameraParameters(P_m, P_g, ...
+                'EstimateSkew', false, 'EstimateTangentialDistortion', false, ...
+                'NumRadialDistortionCoefficients', 2, 'WorldUnits', 'millimeters', ...
+                'InitialIntrinsicMatrix', [], 'InitialRadialDistortion', [], ...
+                'ImageSize', [mrows, ncols]);
+            fprintf('COMPLETE\n\n');
+        case 'fisheye'
+            % Fisheye camera calibration
+            fprintf('\nCalibrating using fisheye camera model...');
+            [camParams, imagesUsed, estimationErrors] = ...
+                estimateFisheyeParameters(P_m, P_g, ...
+                [mrows, ncols], ...
+                'EstimateAlignment', true, ...
+                'WorldUnits', 'millimeters');
+            fprintf('COMPLETE\n\n');
+    end
+catch
+    fprintf('ERROR\n\n');
+    warning('Unable to estimate camera parameters.');
+    return
 end
+
 % Update list of images used
 imageNames = imageNames(imagesUsed);
 % Update list of indices used
@@ -329,7 +249,8 @@ switch lensType
     case 'fisheye'
         fprintf('\n!!! Fisheye Model Selected !!!\n\n')
         fprintf('Fisheye camera model does not provide an intrinsic matrix!\n')
-        fprintf('\tUse imagePoints = worldToImage(cal.camParams.Intrinsics,H_o2c(1:3,1:3).'',H_o2c(1:3,4).'',P_o(1:3,:).'')\n')
+        fprintf('\tTo project points onto the image, use:\n');
+        fprintf('\t\timagePoints = worldToImage(camParams.Intrinsics,H_o2c(1:3,1:3).'',H_o2c(1:3,4).'',P_o(1:3,:).'')\n')
         A_c2m = [];
 end
 
