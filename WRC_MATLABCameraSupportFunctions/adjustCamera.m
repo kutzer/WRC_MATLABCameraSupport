1c40136c8a8d0b787e5b9375f16efd914a4ae43f
Michael Kutzer <kutzer@usna.edu>
2023-11-30 10:39:47 -0500

    Update adjustCamera.m

--- WRC_MATLABCameraSupportFunctions/adjustCamera.m
+++ WRC_MATLABCameraSupportFunctions/adjustCamera.m
@@ -1,15 +1,41 @@
-function adjustCamera(cam)
+function varargout = adjustCamera(cam,varargin)
 % ADJUSTCAMERA allows the user to adjust the settings of a video input
 % object using a basic GUI.
+%   adjustCamera(cam)
+%   camSettings = adjustCamera(cam)
+%   ___ = adjustCamera(cam,camSettings)
+%   ___ = adjustCamera(cam,camSettings,skipGUI)
 %
 %   Input(s)
-%       cam - video input object
+%               cam - video input object
+%       camSettings - [OPTIONAL] structured array containing camera 
+%                     settings. Note that field names must match the camera
+%                     setting property name to be used. Incorrect settings
+%                     are ignored.
+%           skipGUI - [OPTIONAL] allows the user to adjust the camera using
+%                     the camSettings structured array and automatically
+%                     close the GUI. 
 %
-%   Example:
+%   Output(s)
+%       camSettings - [OPTIONAL] strutured array containing the last set of 
+%                     camera settings implemented by the GUI. Note that
+%                     specifying an output will block execution until the
+%                     GUI is closed using the "Exit" button.
+%
+%   Example 1: Basic use
 %       % Initialize camera
 %       [init,cam] = initCamera;
 %       % Adjust camera parameters
 %       adjustCamera(cam);
+%
+%   Example 2: Saving and re-using parameters
+%       % Initialize camera
+%       [init,cam] = initCamera;
+%       % Adjust camera parameters and get settings used
+%       camSettings = adjustCamera(cam);
+%       ...
+%       % Apply camera settings to camera
+%       adjustCamera(cam,camSettings,true);
 %
 %   Recommended steps to adjust an Imaging Source DFK 23U618 with 
 %   mechanical aperture lens using the adjustCamera GUI:
@@ -30,11 +56,17 @@ function adjustCamera(cam)
 % Updates
 %   07Apr2022 - Updated to account for small monitors
 %   22Apr2022 - Added example
+%   30Nov2023 - Added structured array output for camera settings
+%   30Nov2023 - Added user settings input option
+%   30Nov2023 - Added bypass GUI option
+
+% TODO - avoid use of global variable for sharing camera settings
+global adjustCamera_camSettings
 
 debugON = false;
 
 %% Check input(s)
-narginchk(1,1);
+narginchk(1,3);
 switch lower(class(cam))
     case 'videoinput'
         % Input is a video input object
@@ -44,6 +76,9 @@ switch lower(class(cam))
     otherwise
         error('Input must be a valid video input object.');
 end
+
+%% Initialize output(s)
+camSettings = struct;
 
 %% Show preview
 prv = preview(cam);
@@ -151,7 +186,7 @@ for k = 1:n
                         'String',sprintf('%s',prop_vals{k}));
                 case 'enum'
                     list = prop_info{k}.ConstraintValue;
-                    val = find( contains(list,prop_vals{k}) );
+                    val = find( matches(list,prop_vals{k}) );
                     uiC(k) =  uicontrol(uiP(k),'Style','popupmenu',...
                         'Units','Normalized','Position',[0.1,0.1,0.8,0.8],...
                         'String',list,'Value',val,'Tag',prop_names{k});
@@ -170,6 +205,7 @@ fcnApply = @(hObject, eventdata)...
 fcnDefault = @(hObject, eventdata)...
     applyDefault(hObject,eventdata,cam,src_obj,uiC,prop_info,prop_vals);
 fcnDelete = @(hObject, eventdata)delete(fig);
+
 % Define buttons
 uiB(1) = uicontrol(fig,'Style','Pushbutton',...
     'Units','Pixels','Position',[w0,10,wBTN,hBTN]*ratio,...
@@ -181,12 +217,97 @@ uiB(3) = uicontrol(fig,'Style','Pushbutton',...
     'Units','Pixels','Position',[w0+2*(wBTN+5),10,wBTN,hBTN]*ratio,...
     'Tag','Exit','String','Exit','Callback',fcnDelete);
 
+%% Adjust defaults if camSettings is provided
+if nargin > 1
+    camSettings = varargin{1};
+    
+    % Check input
+    if ~isstruct(camSettings)
+        error('Camera settings must be specified as a structured array');
+    end
+    
+    % Get provided property names
+    set_names = fields(camSettings);
+    
+    % Initialize comparison arrays
+    tfSet = false(numel(set_names),1);
+    tfProp = false(1,numel(prop_names));
+    
+    % Compare field names
+    for k = 1:numel(prop_names)
+        tfFind = matches(set_names,prop_names{k});
+        tfSet = tfSet | tfFind;
+        if any(tfFind)
+            tfProp(k) = true;
+            % Update default property value
+            prop_vals{k} = camSettings.(set_names{tfFind});
+        end
+    end
+    
+    % Display comparison results to user
+    if ~all(tfProp)
+        fprintf('\nThe following camera settings were not included in the user-provided camera settings:\n');
+        for k = reshape(find(~tfProp),1,[])
+            fprintf('%20s - ',prop_names{k});
+            
+            if ischar(prop_vals{k})
+                fprintf('[DEFAULT] "%s"\n',prop_vals{k});
+            else
+                fprintf('[DEFAULT] %d\n',prop_vals{k});
+            end
+        end
+    end
+    
+    if ~all(tfSet)
+        fprintf('\nThe following user-provided camera settings are not valid settings for this camera:\n');
+        for k = reshape(find(~tfSet),1,[])
+            fprintf('%20s - ',set_names{k});
+            
+            if ischar(camSettings.(set_names{k}))
+                fprintf('[IGNORED] "%s"\n',camSettings.(set_names{k}));
+            else
+                fprintf('[IGNORED] %d\n',camSettings.(set_names{k}));
+            end
+        end
+    end
+    
+    % Update defaults
+    fprintf('\n>>>>>> Applying user-provided camera settings >>>>>>\n');
+    kids = get(fig,'Children');
+    applyDefault(kids(1),[],cam,src_obj,uiC,prop_info,prop_vals);
+    fprintf('<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n\n');
+end
+
+%% Bypass GUI if user specifies
+if nargin > 2
+    skipGUI = varargin{2};
+    if numel(skipGUI) ~= 1 || ~islogical(skipGUI)
+        warning('The "skip GUI" option must be specified as a scalar logical array (i.e. true or false)');
+    else
+        if skipGUI
+            delete(fig);
+        end
+    end
+end
+
+%% Provide output if required
+if nargout > 0
+    % Wait for execution to finish
+    uiwait(fig);
+    
+    % Recover settings from GUI
+    varargout{1} = adjustCamera_camSettings;
+end
+
 end
 
 %% Internal functions
 function applyCallback(hObject,eventdata,cam,src_obj,uiC,prop_info)
 % hObject
 % eventdata
+
+% TODO - avoid use of global variable for sharing camera settings
+global adjustCamera_camSettings
 
 if ~isvalid(cam)
     warning('Video input object is not associated with any hardware, try reinitializing.');
@@ -200,8 +321,14 @@ fprintf('Stopping Camera...');
 stop(cam);
 fprintf('[COMPLETE]\n');
 fprintf('\tSetting:\n');
+
+% Debug code
+%assignin('base','uiC',uiC);
+
 for k = 1:numel(uiC)
+    % Debug code
     %uiC(k)
+    
     camProp = get(uiC(k),'Tag');
     fprintf('%20s to ',camProp);
     
@@ -244,6 +371,9 @@ for k = 1:numel(uiC)
     catch
             fprintf('UNABLE TO SET, SEE TODO\n');
     end
+    
+    % Update camera settings variable
+    adjustCamera_camSettings.(camProp) = setVal;
 end
 fprintf('Starting Camera...');
 start(cam);
@@ -253,6 +383,9 @@ end
 function applyDefault(hObject,eventdata,cam,src_obj,uiC,prop_info,prop_vals)
 % hObject
 % eventdata
+
+% TODO - avoid use of global variable for sharing camera settings
+global adjustCamera_camSettings
 
 if ~isvalid(cam)
     warning('Video input object is not associated with any hardware, try reinitializing.');
@@ -290,7 +423,7 @@ for k = 1:numel(uiC)
             src_obj.(camProp) = setVal;
         case 'popupmenu'
             list = get(uiC(k),'String');
-            val = find( contains(list,setVal) );
+            val = find( matches(list,setVal) );
             set(uiC(k),'Value',val);
             src_obj.(camProp) = setVal;
         otherwise
@@ -300,12 +433,14 @@ for k = 1:numel(uiC)
             continue
     end
     
-    
     if ischar(setVal)
         fprintf('"%s"\n',setVal);
     else
         fprintf('%d\n',setVal);
     end
+    
+    % Update camera settings variable
+    adjustCamera_camSettings.(camProp) = setVal;
 end
 fprintf('Starting Camera...');
 start(cam);
