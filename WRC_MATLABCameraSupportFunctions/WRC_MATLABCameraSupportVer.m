436cc1549cdcf91bc1028d8d1bf4097da6a95403
Michael Kutzer <kutzer@usna.edu>
2025-03-27 11:00:40 -0400

    Update WRC_MATLABCameraSupportVer.m

--- WRC_MATLABCameraSupportFunctions/WRC_MATLABCameraSupportVer.m
+++ WRC_MATLABCameraSupportFunctions/WRC_MATLABCameraSupportVer.m
@@ -38,11 +38,12 @@ function varargout = WRC_MATLABCameraSupportVer
 %   23Jan2025 - Updated getCalibrationImages to add 
 %               showCheckerboardOnPreview and clearPreview
 %   17Mar2025 - Added isBinaryImage to support legacy functions
+%   27Mar2025 - Updated documentation
 
 A.Name = 'WRC MATLAB Camera Support';
-A.Version = '1.2.5';
+A.Version = '1.2.6';
 A.Release = '(R2019b)';
-A.Date = '17-Mar-2025';
+A.Date = '27-Mar-2025';
 A.URLVer = 1;
 
 msg{1} = sprintf('MATLAB %s Version: %s %s',A.Name, A.Version, A.Release);
