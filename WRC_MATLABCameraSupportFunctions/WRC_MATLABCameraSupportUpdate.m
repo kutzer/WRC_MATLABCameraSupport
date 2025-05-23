682f4ae180faeacd3424fc919035b3c9e8dd280a
Michael Kutzer <kutzer@usna.edu>
2025-05-23 12:57:26 -0400

    Update WRC_MATLABCameraSupportUpdate.m

--- WRC_MATLABCameraSupportFunctions/WRC_MATLABCameraSupportUpdate.m
+++ WRC_MATLABCameraSupportFunctions/WRC_MATLABCameraSupportUpdate.m
@@ -6,8 +6,8 @@ function WRC_MATLABCameraSupportUpdate
 
 % Updates
 %   08Jan2021 - Updated ToolboxUpdate
+%   22May2025 - Updated to enable local install
 
-% TODO - Find a location for Example SCRIPTS
 % TODO - update function for general operation
 
 % Update WRC MATLAB Camera Support
@@ -15,13 +15,15 @@ ToolboxUpdate('WRC_MATLABCameraSupport');
 
 end
 
+%% Internal functions (unique workspace)
+% ------------------------------------------------------------------------
 function ToolboxUpdate(toolboxName)
 
-%% Setup functions
+% Setup functions
 ToolboxVer = str2func( sprintf('%sVer',toolboxName) );
 installToolbox = str2func( sprintf('install%s',toolboxName) );
 
-%% Check current version
+% Check current version
 try
     A = ToolboxVer;
 catch ME
@@ -29,7 +31,7 @@ catch ME
     fprintf('No previous version of %s detected.\n',toolboxName);
 end
 
-%% Setup temporary file directory
+% Setup temporary file directory
 fprintf('Downloading the %s...',toolboxName);
 tmpFolder = sprintf('%s',toolboxName);
 pname = fullfile(tempdir,tmpFolder);
@@ -40,7 +42,7 @@ end
 % Create new directory
 [ok,msg] = mkdir(tempdir,tmpFolder);
 
-%% Download and unzip toolbox (GitHub)
+% Download and unzip toolbox (GitHub)
 url = sprintf('https://github.com/kutzer/%s/archive/master.zip',toolboxName);
 try
     %fnames = unzip(url,pname);
@@ -58,48 +60,72 @@ catch ME
     fprintf(2,'ERROR MESSAGE:\n\t%s\n',ME.message);
 end
 
-%% Check for successful download
+% Check for successful download
 alternativeInstallMsg = [...
     sprintf('Manually download the %s using the following link:\n',toolboxName),...
     newline,...
     sprintf('%s\n',url),...
     newline,...
     sprintf('Once the file is downloaded:\n'),...
-    sprintf('\t(1) Unzip your download of the "%s"\n',toolboxName),...
-    sprintf('\t(2) Change your "working directory" to the location of "install%s.m"\n',toolboxName),...
+    sprintf('\t(1) Unzip your download of the "%sToolbox"\n',toolboxName),...
+    sprintf('\t(2) Change your "working directory" to the location of "install%sToolbox.m"\n',toolboxName),...
     sprintf('\t(3) Enter "install%s" (without quotes) into the command window\n',toolboxName),...
     sprintf('\t(4) Press Enter.')];
         
 if ~confirm
-    warning('InstallToolbox:FailedDownload','Failed to download updated version of %s.',toolboxName);
+    warning('InstallToolbox:FailedDownload','Failed to download updated version of %s Toolbox.',toolboxName);
     fprintf(2,'\n%s\n',alternativeInstallMsg);
 	
     msgbox(alternativeInstallMsg, sprintf('Failed to download %s',toolboxName),'warn');
     return
 end
 
-%% Find base directory
+% Find base directory
 install_pos = strfind(fnames, sprintf('install%s.m',toolboxName) );
 sIdx = cell2mat( install_pos );
 cIdx = ~cell2mat( cellfun(@isempty,install_pos,'UniformOutput',0) );
 
 pname_star = fnames{cIdx}(1:sIdx-1);
 
-%% Get current directory and temporarily change path
+% Get current directory and temporarily change path
 cpath = cd;
 cd(pname_star);
 
-%% Install Toolbox
-installToolbox(true);
+% Check for admin
+skipAdmin = ~checkWriteAccess(matlabroot);
 
-%% Move back to current directory and remove temp file
+% Install Toolbox
+% TODO - consider providing the user with an option or more information
+%        related to "skipAdmin"
+try
+    installToolbox(true,skipAdmin);
+catch ME
+    cd(cpath);
+    throw(ME);
+end
+
+% Move back to current directory and remove temp file
 cd(cpath);
 [ok,msg] = rmdir(pname,'s');
 if ~ok
     warning('Unable to remove temporary download folder. %s',msg);
 end
 
-%% Complete installation
+% Complete installation
 fprintf('Installation complete.\n');
+
+end
+% -------------------------------------------------------------------------
+function tfWrite = checkWriteAccess(pname)
+
+tmpFname = fullfile(pname,'tmp.txt');
+tmpHndle = fopen(tmpFname, 'w');
+if tmpHndle < 0
+    tfWrite = false;
+else
+    tfWrite = true;
+    fclose(tmpHndle);
+    delete(tmpFname);
+end
 
 end
